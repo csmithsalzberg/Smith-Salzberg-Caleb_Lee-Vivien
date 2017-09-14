4f9e8764f6c993d852dfdff9258023309ce65b43
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2017-09-14 12:14:20 -0400

    started

---
+++ jobs.py
@@ -0,0 +1,10 @@
+dict={}
+file = open('occupations.csv','r')
+ugly = file.read()
+lines = ugly.split("\n")
+for line in lines:
+    if line[0]=='"'
+
+    else:
+        d[line.split(",")][0]
+    if close[0]=='"'
