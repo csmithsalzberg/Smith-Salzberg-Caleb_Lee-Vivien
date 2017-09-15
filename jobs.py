07543961c581d7accd159094989f01d7d38c5360
vivienlee00 <vivien.lee.601@gmail.com>
2017-09-14 23:39:04 -0400

    properly reads csv into dictionary, need to create weighting fxn

--- jobs.py
+++ jobs.py
@@ -1,10 +1,19 @@
+#initialize dictionary and read in csv file
 dict={}
 file = open('occupations.csv','r')
 ugly = file.read()
 lines = ugly.split("\n")
-for line in lines:
-    if line[0]=='"'
 
+#delete unnecessary lines from csv file
+del lines[0]
+del lines[len(lines)-1]
+del lines[len(lines)-1]
+
+#adding each line to dictionary
+for line in lines:
+    if line[0]=="\"":
+        dict[line[0:(line.index("\"",1)+1)]] = line[(line.index("\"",1)+2):]
     else:
-        d[line.split(",")][0]
-    if close[0]=='"'
+        dict[line.split(",")[0]]=line.split(",")[1]
+
+print(dict)
